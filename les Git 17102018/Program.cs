767377bf03324618f4f5676d11991d13fa2bc76c
Jurgen <43148753+jbostyn@users.noreply.github.com>
2018-10-17 19:46:49 +0200

    Add project files.

---
+++ les Git 17102018/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace les_Git_17102018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
